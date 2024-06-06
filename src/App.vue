131c9f244503ab45dda1a34184f0451f84bed5dc
Emiliana Manzo <152317309+EmilianaManzo@users.noreply.github.com>
2024-06-06 11:22:29 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+  import axios from 'axios';
+  import Header from './components/Header.vue';
+  import Main from './components/Main.vue';
+  import Footer from './components/Footer.vue';
+
+  export default {
+      components:{
+        Header,
+        Main,
+        Footer,
+      },
+      data(){
+        return{
+          axios
+        }
+      }
+  }
+</script>
+
+
+<template>
+  
+</template>
+
+
+<style lang="scss">
+@use './assets/scss/main.scss'
+
+</style>
